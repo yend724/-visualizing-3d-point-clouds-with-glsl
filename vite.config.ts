35040b1257ee99b471b7705f7f4b9627d76d5e2b
yend724 <yend9524@gmail.com>
2024-02-23 13:43:49 +0900

    Add base URL and deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/visualizing-3d-point-clouds-with-glsl/',
   plugins: [react()],
-})
+});
